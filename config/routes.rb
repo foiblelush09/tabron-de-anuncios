1744d5fa849302dcfe3738e81211a95b923133de
foiblelush09 <fcb.football.1015@gmail.com>
2021-10-14 17:50:07 +0900

    募集投稿ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
     devise_for :users
     root to: 'boards#index'
-    resources :boards, only: :index
+    resources :boards, only: [:index, :new]
   
   end
 
